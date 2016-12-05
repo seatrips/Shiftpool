7fd77d89de5e29e34b80b78842a5e9aedb09c5b7
Unhandled Exception <karek314@protonmail.ch>
2016-12-05 19:44:33 +0100

    Update stats.php
    fixed missing stats for individual forgers

--- private/stats.php
+++ private/stats.php
@@ -1,14 +1,12 @@
 <?php
   error_reporting(error_reporting() & ~E_NOTICE & ~E_WARNING);
   $config = include('../config.php');
-
   $df = 0;
   $delegate = $config['delegate_address'];
   $lisk_host = $config['lisk_host'];
   $lisk_port = $config['lisk_port'];
   $pool_fee = floatval(str_replace('%', '', $config['pool_fee']));
   $pool_fee_payout_address = $config['pool_fee_payout_address'];
-
 while(1) {
   $df++;
   $start_time = time();
@@ -21,7 +19,6 @@ while(1) {
   $publicKey_json = json_decode($result1, true); 
   $publicKey = $publicKey_json['account']['publicKey'];
   $pool_balance = $publicKey_json['account']['balance'];
-
   //get forging delegate info
   $ch1 = curl_init('http://'.$lisk_host.':'.$lisk_port.'/api/delegates/get/?publicKey='.$publicKey);
   curl_setopt($ch1, CURLOPT_CUSTOMREQUEST, "GET");                                                                                      
@@ -30,7 +27,6 @@ while(1) {
   $d_data = json_decode($result1, true); 
   $d_data = $d_data['delegate'];
   $rank = $d_data['rate'];
-
   //Retrive voters
   $ch1 = curl_init('http://'.$lisk_host.':'.$lisk_port.'/api/delegates/voters?publicKey='.$publicKey);
   curl_setopt($ch1, CURLOPT_CUSTOMREQUEST, "GET");                                                                                      
@@ -50,17 +46,13 @@ while(1) {
     $total_voters_power_d = $total_voters_power/100000000000000;
     $add2Stats = "INSERT INTO pool_votepower (votepower, val_timestamp) VALUES ('$total_voters_power_d', '$cur_time')";
     $querydone = mysqli_query($mysqli,$add2Stats) or die("Database Error 0");
-
     $balanceinlsk_p = floatval($pool_balance/100000000);
     $add2Stats = "INSERT INTO pool_balance (value, var_timestamp) VALUES ('$balanceinlsk_p', '$cur_time')";
     $querydone = mysqli_query($mysqli,$add2Stats) or die("Database Error 0");
-
     $add2Stats = "INSERT INTO pool_voters (value, var_timestamp) VALUES ('$voters_count', '$cur_time')";
     $querydone = mysqli_query($mysqli,$add2Stats) or die("Database Error 0");
-
     $add2Stats = "INSERT INTO pool_rank (value, var_timestamp) VALUES ('$rank', '$cur_time')";
     $querydone = mysqli_query($mysqli,$add2Stats) or die("Database Error 0");
-
     $db_users_count = 0;
     $users_data = '';
     $existQuery = "SELECT address,balance FROM miners";
@@ -69,13 +61,12 @@ while(1) {
       $val1 = $row[0];
       $val2 = $row[1];
       $balanceinlsk = floatval($val2/100000000);
-      $users_data = $users_data.' '."('$val1', '$balanceinlsk', '$cur_time'),";
-      if (!$val1) {
-        $users_data = 'empty';
+      if ($balanceinlsk != 0) {
+        $users_data = $users_data.' '."('$val1', '$balanceinlsk', '$cur_time'),";
       }
       $db_users_count++;
     }
-    if ($users_data == 'empty') {
+    if ($users_data != '') {
       $mysqli=mysqli_connect($config['host'], $config['username'], $config['password'], $config['bdd']) or die(mysqli_error($mysqli));
       $users_data = substr($users_data, 0, -1);
       $add2Stats = "INSERT INTO miner_balance (miner, value, var_timestamp) VALUES".$users_data;
@@ -102,5 +93,4 @@ while(1) {
     echo "Can't get data...";
   }
 }
-
 ?>
